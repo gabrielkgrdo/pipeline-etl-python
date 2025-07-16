a2acf1b3a23b55a074f8e51353db287c63ba7afe
gabrielkgrdo <gabrielderoque@gmail.com>
2025-07-16 15:28:41 -0300

    feat: adiciona frontend com dataframe das commodities utilizando streamlit

---
+++ app/app.py
@@ -0,0 +1,49 @@
+import os
+import pandas as pd
+import streamlit as st
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obter as variáveis do arquivo .env
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASS = os.getenv('DB_PASS_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# Criar a URL de conexão do banco de dados
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Criar o engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+
+def obter_dados():
+    query = f"""
+    Select 
+        data,
+        simbolo,
+        valor_fechamento,
+        acao,
+        quantidade,
+        valor,
+        ganho
+    FROM
+        public.dm_commodities;
+    """
+    df = pd.read_sql(query, engine)
+    return df
+
+st.set_page_config(page_title="Dashboard Commodities", layout='centered')
+
+st.title('Relatório sobre as commodities')
+
+st.write('Esse relatório mostra dados de commodities extraídos de uma api e suas transações extraídas de um csv.')
+
+df = obter_dados()
+
+st.dataframe(df)
